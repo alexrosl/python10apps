6a82aaf2bc607fdd843def58a1af876627c4cf38
alexrosl <reshetnikov.alex5@yandex.ru>
2020-05-31 13:55:28 +0300

    Add app2 website blocker

---
+++ section19_website_blocker/website_blocker.py
@@ -0,0 +1,30 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp = "hosts"
+hosts_path = r'/etc/hosts'
+redirect = '127.0.0.1'
+website_list = ['wwww.facebook.com', 'facebook.com', "dub.19.mail.live.com", "www.dub119.mail.live.com"]
+
+while True:
+    if dt(dt.now().year, dt.now().month, dt.now().day, 8) < \
+            dt.now() < \
+            dt(dt.now().year, dt.now().month, dt.now().day, 16):
+        print("working hours")
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                else:
+                    file.write(redirect + " " + website + "\n")
+    else:
+        with open(hosts_temp, 'r+') as file:
+            content = file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
+        print("fun hours")
+    time.sleep(5)
